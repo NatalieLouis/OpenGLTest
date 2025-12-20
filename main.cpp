4e891bb98b06d6fbb81004a5ffab9e0020c4f1f4
NatalieLouis <781798334@qq.com>
2025-12-20 14:33:29 +0800

    添加项目文件。

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+using namespace std;
+int main() {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	auto window = glfwCreateWindow(800, 600, "OpenglLearn", NULL, NULL);
+	glfwMakeContextCurrent(window);
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+	}
+	glfwTerminate();
+	return 0;
+}
