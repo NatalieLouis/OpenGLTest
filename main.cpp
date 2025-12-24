8666fb0185b488fa9ab950a3c4abcb2434010378
NatalieLouis <781798334@qq.com>
2025-12-25 00:17:10 +0800

    vao with single buffer

--- main.cpp
+++ main.cpp
@@ -14,10 +14,35 @@ void prepare() {
 		0.5,-0.5,0,
 		0,0.5,0
 	};
-	GLuint vbo = 0;
+	float color[] = {
+		1,0,0,
+		0,1,0,
+		0,0,1
+	};
+	GLuint vbo , color_vbo;
 	glGenBuffers(1, &vbo);
+	glGenBuffers(1, &color_vbo);
+
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices),vertices,GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ARRAY_BUFFER, color_vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(color), color, GL_STATIC_DRAW);
+
+	GLuint vao = 0;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, color_vbo);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+
+	glBindVertexArray(0);//½â°óvao
+
 }
 int main() {
 	glfwInit();
@@ -26,7 +51,16 @@ int main() {
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	auto window = glfwCreateWindow(800, 600, "OpenglLearn", NULL, NULL);
 	glfwMakeContextCurrent(window);
+	
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		cerr << "Failed to initialize GLAD!" << endl;
+		return -1;
+	}
+	prepare();
 	while (!glfwWindowShouldClose(window)) {
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 	glfwTerminate();
