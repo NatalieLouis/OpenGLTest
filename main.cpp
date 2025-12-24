3eb51b9b4c6c983ed1b0e16bda3d67939a275915
NatalieLouis <781798334@qq.com>
2025-12-24 23:29:00 +0800

    intit window gui

--- main.cpp
+++ main.cpp
@@ -3,6 +3,22 @@
 #include <GLFW/glfw3.h>
 
 using namespace std;
+void prepareVBO() {
+	GLuint vbo = 0;
+	glGenBuffers(1, &vbo);
+	glDeleteBuffers(1, &vbo);
+}
+void prepare() {
+	float vertices[] = {
+		-0.5,-0.5,0,
+		0.5,-0.5,0,
+		0,0.5,0
+	};
+	GLuint vbo = 0;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices),vertices,GL_STATIC_DRAW);
+}
 int main() {
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
